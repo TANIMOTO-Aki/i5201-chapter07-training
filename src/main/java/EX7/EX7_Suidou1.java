26b36a6f0e45df77a1e71278f153b6caf425c86b
TANIMOTO-Aki <kisy23011@kibireha.ac.jp>
2023-12-05 13:40:09 +0900

    イシューの内容を元に、定数の使用とif文の修正を行いました。

--- src/main/java/EX7/EX7_Suidou1.java
+++ src/main/java/EX7/EX7_Suidou1.java
@@ -3,20 +3,24 @@ import java.io.*;
 public class EX7_Suidou1 {
     public static void main(String[] args) throws IOException {
         BufferedReader br =
-            new BufferedReader(new InputStreamReader(System.in));
+                new BufferedReader(new InputStreamReader(System.in));
+        final int MIN_USE = -1;
+        final int USAGE_RATE = 50;              //従量料金
 
         /* 使用量に-1が入力されるまで処理を繰り返す */
         while(true) {
             System.out.print("使用量？ ");
             int use = Integer.parseInt(br.readLine());
-            if (use == -1) {
+            if (use == MIN_USE) {
                 break;
-            } else if (use <= 50) {
-                int amo = 1000 + 80 * use;
-                System.out.println("料金=" + amo +"\n");
-            } else if (use > 50) {
-                int amo = 5000 + 120 * (use - 50);
-                System.out.println("料金=" + amo +"\n");
+            }
+
+            if (use <= USAGE_RATE) {
+                int amount = 1000 + 80 * use;
+                System.out.println("料金=" + amount +"\n");
+            } else {
+                int amount = 5000 + 120 * (use - USAGE_RATE);
+                System.out.println("料金=" + amount +"\n");
             }
         }
     }
