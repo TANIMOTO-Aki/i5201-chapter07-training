4da0c460bcdc49a89f11c279fc1fab6d6a4feb9e
TANIMOTO-Aki <kisy23011@kibireha.ac.jp>
2023-12-05 13:54:50 +0900

    イシュー内容を元に、定数の使用と、重複コードの削除を行いました。

--- src/main/java/EX7/EX7_Suidou2.java
+++ src/main/java/EX7/EX7_Suidou2.java
@@ -3,42 +3,40 @@ import java.io.*;
 public class EX7_Suidou2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br =
-            new BufferedReader(new InputStreamReader(System.in));
+                new BufferedReader(new InputStreamReader(System.in));
+        final int MIN_USE = 0;
+        final int USAGE_RATE = 50;              //従量料金
 
         /* 区分に0が入力されるまで繰り返す */
         suidou:
         while(true) {
-            int use = 0,amo = 0;
+            int use = 0,amount = 0;
             System.out.print("区分？　　");
             int section = Integer.parseInt(br.readLine());
+            if (section == MIN_USE) {
+                break;
+            }
+            System.out.print("使用量？　");
+            use = Integer.parseInt(br.readLine());
             switch (section) {
-            case 0:
-                break suidou;
             case 1:
-                System.out.print("使用量？　");
-                use = Integer.parseInt(br.readLine());
-                if (use <= 50) {
-                    amo = 1000 + 80 * use;
-                    System.out.println("一般－－－－料金=" + amo +"\n");
-                } else if (use > 50) {
-                    amo = 5000 + 120 * (use - 50);
-                    System.out.println("一般－－－－料金=" + amo +"\n");
+                if (use <= USAGE_RATE) {
+                    amount = 1000 + 80 * use;
+                } else if (use > USAGE_RATE) {
+                    amount = 5000 + 120 * (use - USAGE_RATE);
                 }
+                System.out.println("一般－－－－料金=" + amount +"\n");
                 break;
             case 2:
-                System.out.print("使用量？　");
-                use = Integer.parseInt(br.readLine());
-                amo = 1300 + 150 * use;
-                System.out.println("営業－－－－料金=" + amo +"\n");
+                amount = 1300 + 150 * use;
+                System.out.println("営業－－－－料金=" + amount +"\n");
                 break;
             case 3:
-                System.out.print("使用量？　");
-                use = Integer.parseInt(br.readLine());
-                amo = 2300 + 240 * use;
-                System.out.println("臨時－－－－料金=" + amo +"\n");
+                amount = 2300 + 240 * use;
+                System.out.println("臨時－－－－料金=" + amount +"\n");
                 break;
             default:
-                System.out.println("1-3の整数を入力してください" + "\n" + "終了する場合は0を入力します");
+                System.out.println("区分には1-3の整数を入力してください" + "\n" + "終了する場合は0を入力します");
                 break;
             }
         }
