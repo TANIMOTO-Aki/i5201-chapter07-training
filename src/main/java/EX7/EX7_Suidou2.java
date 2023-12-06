4f0e80a76731fd07d74e77f0307f10d2f4441be3
TANIMOTO-Aki <kisy23011@kibireha.ac.jp>
2023-12-06 10:17:25 +0900

    イシュー内容を元に修正中ですが、区分に1を入力したところ、正しい挙動になりません。 挙動的にcase1ではなく、case2のものが適応されているのだと思います。

--- src/main/java/EX7/EX7_Suidou2.java
+++ src/main/java/EX7/EX7_Suidou2.java
@@ -4,40 +4,59 @@ public class EX7_Suidou2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br =
                 new BufferedReader(new InputStreamReader(System.in));
-        final int MIN_USE = 0;
-        final int USAGE_RATE = 50;              //従量料金
+
+        final int EXIT_SIGNAL = 0;
+        final int USAGE_THRESHOLD = 50;              //一般のしきい値
+        final int GENERAL_BASE_RATE = 1000;          //一般の基本料金
+        final int GENERAL_USAGE_RATE1 = 80;          //一般の従量料金(使用量50㎡まで)
+        final int GENERAL_USAGE_RATE2 = 120;         //一般の従量料金(使用量50㎡を超えるとき)
+        
+        final int COMMERCIAL_BASE_RATE = 1300;       //営業の基本料金
+        final int COMMERCIAL_USAGE_RATE = 150;       //営業の従量料金
+
+        final int TEMPORARY_BASE_RATE = 2300;       //臨時の基本料金
+        final int TEMPORARY_USAGE_RATE = 240;       //臨時の従量料金
+
 
         /* 区分に0が入力されるまで繰り返す */
         while(true) {
             int use = 0,amount = 0;
+            int baseRate = 0, usageRate = 0;
+            String kubun = "";
             System.out.print("区分？　　");
             int section = Integer.parseInt(br.readLine());
-            if (section == MIN_USE) {
+            if (section == EXIT_SIGNAL) {
                 break;
             }
             System.out.print("使用量？　");
             use = Integer.parseInt(br.readLine());
             switch (section) {
             case 1:
-                if (use <= USAGE_RATE) {
-                    amount = 1000 + 80 * use;
-                } else if (use > USAGE_RATE) {
-                    amount = 5000 + 120 * (use - USAGE_RATE);
+                if (use <= USAGE_THRESHOLD) {
+                    baseRate = GENERAL_BASE_RATE;
+                    usageRate = GENERAL_USAGE_RATE1;
+                } else {
+                    baseRate = GENERAL_BASE_RATE + GENERAL_USAGE_RATE1 * USAGE_THRESHOLD;
+                    usageRate = GENERAL_USAGE_RATE2;
+                    use -= USAGE_THRESHOLD;
                 }
-                System.out.println("一般－－－－料金=" + amount +"\n");
-                break;
+                kubun = "一般";
             case 2:
-                amount = 1300 + 150 * use;
-                System.out.println("営業－－－－料金=" + amount +"\n");
+                baseRate = COMMERCIAL_BASE_RATE;
+                usageRate = COMMERCIAL_USAGE_RATE;
+                kubun = "営業";
                 break;
             case 3:
-                amount = 2300 + 240 * use;
-                System.out.println("臨時－－－－料金=" + amount +"\n");
+                baseRate = TEMPORARY_BASE_RATE;
+                usageRate = TEMPORARY_USAGE_RATE;
+                kubun = "臨時";
                 break;
             default:
                 System.out.println("区分には1-3の整数を入力してください" + "\n" + "終了する場合は0を入力します");
                 break;
             }
+            amount = baseRate + use * usageRate;
+            System.out.println(kubun + "－－－－料金=" + amount);
         }
     }
 }
